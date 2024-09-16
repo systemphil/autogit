c2e8abf52bcc14386055ea2892de4a9a4d4e2fc5
Filip Niklas <awdeegrays@gmail.com>
2024-09-16 17:44:36 +0200

    test setup

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World! Rust❤️‍🔥" }));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
