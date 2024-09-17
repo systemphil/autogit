75d79a1e876ee807615d62ffb5e072d04c4e0ed4
Filip Niklas <awdeegrays@gmail.com>
2024-09-17 22:42:01 +0200

    fix: missing code

--- start.sh
+++ start.sh
@@ -2,13 +2,20 @@
 
 # Git & GitHub setup
 if [ -n "$GH_PAT" -a -n "$SSH_KEY" ]; then
+  mkdir -p ~/.ssh
+  # Add the private key from the environment variable
+  echo "$SSH_KEY" > ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+
+  # Start the ssh-agent and add the private key
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
   echo "$GH_PAT" | gh auth login --with-token
   gh auth status
   git config --global user.name "Autogit"
   git config --global user.email "service@systemphil.com"
-  git config --global credential.helper store
-  gh repo clone systemphil/sphil
-  ( cd sphil ; git remote set-url origin https://$GH_PAT@github.com/systemphil/sphil.git )
+  git clone git@github.com:systemphil/sphil.git
 else
   echo "GH_PAT or SSH_KEY environment variable is not set."
   exit 1
