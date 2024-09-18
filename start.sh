2875f685bfa39bfc744dc5fa1ef252dd1b7c991a
Filip Niklas <awdeegrays@gmail.com>
2024-09-18 14:33:04 +0200

    debug: print head and tail of config

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ if [ -n "$GH_PAT" -a -n "$SSH_KEY" -a -n "$KNOWN_HOSTS" ]; then
   mkdir -p ~/.ssh
   # Add the private key from the environment variable
   echo "$SSH_KEY" > ~/.ssh/id_rsa
+  head ~/.ssh/id_rsa
+  tail ~/.ssh/id_rsa
   echo "$KNOWN_HOSTS" >> ~/.ssh/known_hosts
   echo "Host *" >> ~/.ssh/config && echo "    StrictHostKeyChecking no" >> ~/.ssh/config
 
