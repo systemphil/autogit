48413e06385b0b0dea828bfb2ae10aa98a5fb7f8
Filip Niklas <awdeegrays@gmail.com>
2024-09-18 20:48:01 +0200

    even more debug logs

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ -n "$GH_PAT" -a -n "$SSH_KEY" -a -n "$KNOWN_HOSTS" ]; then
   mkdir -p ~/.ssh
   # Add the private key from the environment variable
   echo "$SSH_KEY" > ~/.ssh/id_rsa
-  echo "$KNOWN_HOSTS" >> ~/.ssh/known_hosts
+  echo "$KNOWN_HOSTS" > ~/.ssh/known_hosts
   echo "Host *" >> ~/.ssh/config && echo "    StrictHostKeyChecking no" >> ~/.ssh/config
   cat ~/.ssh/config
   export GIT_SSH_COMMAND='ssh -Tv'
@@ -18,11 +18,13 @@ if [ -n "$GH_PAT" -a -n "$SSH_KEY" -a -n "$KNOWN_HOSTS" ]; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
+  ssh -Tv git@github.com
+
   echo "$GH_PAT" | gh auth login --with-token
   gh auth status
   git config --global user.name "Autogit"
   git config --global user.email "service@systemphil.com"
-  git clone git@github.com:systemphil/sphil.git
+  GIT_SSH_COMMAND='ssh -v' git clone git@github.com:systemphil/sphil.git
 else
   echo "Environment variables are not set."
   exit 1
